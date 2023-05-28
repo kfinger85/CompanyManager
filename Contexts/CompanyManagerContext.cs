2ad6e8d1e114fbc339141c84793c3d06b9eb3e99
Kevin Finger <Kevin.Finger@aiworldwide.com>
2023-05-28 13:20:21 -0600

    Working Context!

--- Contexts/CompanyManagerContext.cs
+++ Contexts/CompanyManagerContext.cs
@@ -1,9 +1,5 @@
-using Microsoft.EntityFrameworkCore;
 using CompanyManager.Models;
-using Pomelo.EntityFrameworkCore.MySql;
-using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
-
-
+using Microsoft.EntityFrameworkCore;
 
 public class CompanyManagerContext : DbContext
 {
@@ -11,148 +7,57 @@ public class CompanyManagerContext : DbContext
     public DbSet<Project> Projects { get; set; }
     public DbSet<Qualification> Qualifications { get; set; }
     public DbSet<Worker> Workers { get; set; }
-    public DbSet<WorkerProject> WorkerProjects { get; set; }
 
-        public CompanyManagerContext(DbContextOptions<CompanyManagerContext> options) : base(options)
-        {
-        }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    public CompanyManagerContext(DbContextOptions<CompanyManagerContext> options) : base(options)
     {
-
+        this.Database.EnsureDeleted();  // drop the database if it exists
+        this.Database.EnsureCreated();  // create the database
     }
 
-
-protected override void OnModelCreating(ModelBuilder modelBuilder)
-{
-      // Configure CompanyQualification entity
-    modelBuilder.Entity<CompanyQualification>()
-        .HasKey(cq => new { cq.CompanyId, cq.QualificationId });
-
-    modelBuilder.Entity<CompanyQualification>()
-        .ToTable("CompanyQualifications"); // Specify the table name explicitly
-
-    modelBuilder.Entity<CompanyQualification>()
-        .HasOne(cq => cq.Company)
-        .WithMany()
-        .HasForeignKey(cq => cq.CompanyId);
-
-    modelBuilder.Entity<CompanyQualification>()
-        .HasOne(cq => cq.Qualification)
-        .WithMany()
-        .HasForeignKey(cq => cq.QualificationId);
-
-    // Configure ProjectQualification entity
-    modelBuilder.Entity<ProjectQualification>()
-        .HasKey(pq => new { pq.ProjectId, pq.QualificationId });
-
-            modelBuilder.Entity<ProjectQualification>()
-                .ToTable("ProjectQualification"); // Specify the table name explicitly
-
-    modelBuilder.Entity<ProjectQualification>()
-        .HasOne(pq => pq.Project)
-        .WithMany()
-        .HasForeignKey(pq => pq.ProjectId);
-
-    modelBuilder.Entity<ProjectQualification>()
-        .HasOne(pq => pq.Qualification)
-        .WithMany()
-        .HasForeignKey(pq => pq.QualificationId);
-
-    // Configure WorkerQualification entity
-    modelBuilder.Entity<WorkerQualification>()
-        .HasKey(wq => new { wq.WorkerId, wq.QualificationId });
-
-    modelBuilder.Entity<WorkerQualification>()
-        .HasOne(wq => wq.Worker)
-        .WithMany(w => w.WorkerQualifications)
-        .HasForeignKey(wq => wq.WorkerId);
-
-    modelBuilder.Entity<WorkerQualification>()
-        .HasOne(wq => wq.Qualification)
-        .WithMany(q => q.WorkerQualifications)
-        .HasForeignKey(wq => wq.QualificationId);
-
-    // Configure WorkerProject entity
-    modelBuilder.Entity<WorkerProject>()
-        .HasKey(wp => new { wp.WorkerId, wp.ProjectId });
-
-            modelBuilder.Entity<WorkerProject>()
-        .ToTable("WorkerProject"); // Specify the table name explicitly
-
-    modelBuilder.Entity<WorkerProject>()
-        .HasOne(wp => wp.Worker)
-        .WithMany(w => w.WorkerProjects)
-        .HasForeignKey(wp => wp.WorkerId);
-
-    modelBuilder.Entity<WorkerProject>()
-        .HasOne(wp => wp.Project)
-        .WithMany(p => p.WorkerProjects)
-        .HasForeignKey(wp => wp.ProjectId);
-}
-
-    private void WorkerModelCreate(ModelBuilder modelBuilder)
+    public CompanyManagerContext()
     {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Configure Company
+        modelBuilder.Entity<Company>()
+            .HasMany(c => c.Workers)
+            .WithOne(w => w.Company) // Add the navigation property here
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Company>()
+            .HasMany(c => c.Qualifications)
+            .WithMany(q => q.Companies);
+
+        modelBuilder.Entity<Company>()
+            .HasMany(c => c.Projects)
+            .WithOne(p => p.Company) // Add the navigation property here
+            .OnDelete(DeleteBehavior.Cascade);
+
+
+        // Configure Worker
         modelBuilder.Entity<Worker>()
-            .Property(w => w.Name)
-            .IsRequired();
-
-        modelBuilder.Entity<Worker>()
-            .Property(w => w.Salary)
-            .IsRequired();
-
-        modelBuilder.Entity<Worker>()
-            .Property(w => w.Username)
-            .IsRequired();
-
-        modelBuilder.Entity<Worker>()
-            .Property(w => w.Password)
-            .IsRequired();
-
-modelBuilder.Entity<Worker>()
-    .HasMany(w => w.Projects)
-    .WithMany()
-    .UsingEntity<WorkerProject>(
-        j => j.HasOne(wp => wp.Project)
-            .WithMany()
-            .HasForeignKey(wp => wp.ProjectId),
-        j => j.HasOne(wp => wp.Worker)
-            .WithMany()
-            .HasForeignKey(wp => wp.WorkerId),
-        j =>
-        {
-            j.HasKey(wp => new { wp.WorkerId, wp.ProjectId });
-            j.ToTable("WorkerProject");
-        }
-    );
-
-            
-
-modelBuilder.Entity<Worker>()
-    .HasMany(w => w.Qualifications)
-    .WithMany()
-    .UsingEntity<WorkerQualification>(
-        j => j.HasOne(wq => wq.Qualification)
-            .WithMany()
-            .HasForeignKey(wq => wq.QualificationId),
-        j => j.HasOne(wq => wq.Worker)
-            .WithMany()
-            .HasForeignKey(wq => wq.WorkerId),
-        j =>
-        {
-            j.HasKey(wq => new { wq.WorkerId, wq.QualificationId });
-            j.ToTable("WorkerQualification");
-        }
-    );
+            .HasMany(w => w.Projects)
+            .WithMany(p => p.Workers);
 
         modelBuilder.Entity<Worker>()
-            .HasMany(w => w.WorkerProjects)
-            .WithOne(wp => wp.Worker)
-            .HasForeignKey(wp => wp.WorkerId);
+            .HasMany(w => w.Qualifications)
+            .WithMany(q => q.Workers);
 
         modelBuilder.Entity<Worker>()
-            .HasOne(w => w.Company)
-            .WithMany(c => c.Workers)
-            .HasForeignKey(w => w.CompanyId);
+              .HasOne(w => w.Company) // declares a relationship to Company
+              .WithMany(c => c.Workers) // declares that Company has many Workers
+              // .HasForeignKey(w => w.CompanyId) // explicitly sets the foreign key to CompanyId
+              .OnDelete(DeleteBehavior.Cascade); // or whatever delete behavior you want
+
+        modelBuilder.Entity<Project>()
+            .HasOne(p => p.Company)
+            .WithMany(c => c.Projects)
+            // .HasForeignKey(p => p.CompanyId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<WorkerQualification>().HasKey(wq => new { wq.WorkerId, wq.QualificationId });
+
     }
 }
