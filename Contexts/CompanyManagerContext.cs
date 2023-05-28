a504d80f416ac0a18d7d7dcfc7f51871a4f13abe
Kevin Finger <kfinger85@gmail.com>
2023-05-27 18:14:35 -0600

    Lots and lots of changes to CompMangerContext

--- Contexts/CompanyManagerContext.cs
+++ Contexts/CompanyManagerContext.cs
@@ -1,5 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using CompanyManager.Models;
+using Pomelo.EntityFrameworkCore.MySql;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+
+
 
 public class CompanyManagerContext : DbContext
 {
@@ -9,26 +13,86 @@ public class CompanyManagerContext : DbContext
     public DbSet<Worker> Workers { get; set; }
     public DbSet<WorkerProject> WorkerProjects { get; set; }
 
+        public CompanyManagerContext(DbContextOptions<CompanyManagerContext> options) : base(options)
+        {
+        }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseMySQL("server=localhost;database=CompanyManger;user=root;password=root");
+
     }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        CompanyModelCreate(modelBuilder);
-        modelBuilder.Entity<CompanyQualification>()
-            .HasKey(cq => new { cq.CompanyId, cq.QualificationId });
 
-        modelBuilder.Entity<ProjectQualification>()
-            .HasKey(pq => new { pq.ProjectId, pq.QualificationId });
+protected override void OnModelCreating(ModelBuilder modelBuilder)
+{
+      // Configure CompanyQualification entity
+    modelBuilder.Entity<CompanyQualification>()
+        .HasKey(cq => new { cq.CompanyId, cq.QualificationId });
 
-        modelBuilder.Entity<WorkerQualification>()
-            .HasKey(wq => new { wq.WorkerId, wq.QualificationId });
+    modelBuilder.Entity<CompanyQualification>()
+        .ToTable("CompanyQualifications"); // Specify the table name explicitly
 
-        modelBuilder.Entity<WorkerProject>()
-            .HasKey(wp => new { wp.WorkerId, wp.ProjectId });
-    }
+    modelBuilder.Entity<CompanyQualification>()
+        .HasOne(cq => cq.Company)
+        .WithMany()
+        .HasForeignKey(cq => cq.CompanyId);
+
+    modelBuilder.Entity<CompanyQualification>()
+        .HasOne(cq => cq.Qualification)
+        .WithMany()
+        .HasForeignKey(cq => cq.QualificationId);
+
+    // Configure ProjectQualification entity
+    modelBuilder.Entity<ProjectQualification>()
+        .HasKey(pq => new { pq.ProjectId, pq.QualificationId });
+
+            modelBuilder.Entity<ProjectQualification>()
+                .ToTable("ProjectQualification"); // Specify the table name explicitly
+
+    modelBuilder.Entity<ProjectQualification>()
+        .HasOne(pq => pq.Project)
+        .WithMany()
+        .HasForeignKey(pq => pq.ProjectId);
+
+    modelBuilder.Entity<ProjectQualification>()
+        .HasOne(pq => pq.Qualification)
+        .WithMany()
+        .HasForeignKey(pq => pq.QualificationId);
+
+    // Configure WorkerQualification entity
+    modelBuilder.Entity<WorkerQualification>()
+        .HasKey(wq => new { wq.WorkerId, wq.QualificationId });
+
+    modelBuilder.Entity<WorkerQualification>()
+        .ToTable("WorkerQualification"); // Specify the table name explicitly
+
+    modelBuilder.Entity<WorkerQualification>()
+        .HasOne(wq => wq.Worker)
+        .WithMany()
+        .HasForeignKey(wq => wq.WorkerId);
+
+    modelBuilder.Entity<WorkerQualification>()
+        .HasOne(wq => wq.Qualification)
+        .WithMany()
+        .HasForeignKey(wq => wq.QualificationId);
+
+    // Configure WorkerProject entity
+    modelBuilder.Entity<WorkerProject>()
+        .HasKey(wp => new { wp.WorkerId, wp.ProjectId });
+
+            modelBuilder.Entity<WorkerProject>()
+        .ToTable("WorkerProject"); // Specify the table name explicitly
+
+    modelBuilder.Entity<WorkerProject>()
+        .HasOne(wp => wp.Worker)
+        .WithMany(w => w.WorkerProjects)
+        .HasForeignKey(wp => wp.WorkerId);
+
+    modelBuilder.Entity<WorkerProject>()
+        .HasOne(wp => wp.Project)
+        .WithMany(p => p.WorkerProjects)
+        .HasForeignKey(wp => wp.ProjectId);
+}
 
     private void WorkerModelCreate(ModelBuilder modelBuilder)
     {
@@ -48,15 +112,41 @@ public class CompanyManagerContext : DbContext
             .Property(w => w.Password)
             .IsRequired();
 
-        modelBuilder.Entity<Worker>()
-            .HasMany(w => w.Projects)
-            .WithOne(p => p.Workers)
-            .HasForeignKey(p => p.WorkerId);
+modelBuilder.Entity<Worker>()
+    .HasMany(w => w.Projects)
+    .WithMany()
+    .UsingEntity<WorkerProject>(
+        j => j.HasOne(wp => wp.Project)
+            .WithMany()
+            .HasForeignKey(wp => wp.ProjectId),
+        j => j.HasOne(wp => wp.Worker)
+            .WithMany()
+            .HasForeignKey(wp => wp.WorkerId),
+        j =>
+        {
+            j.HasKey(wp => new { wp.WorkerId, wp.ProjectId });
+            j.ToTable("WorkerProject");
+        }
+    );
 
-        modelBuilder.Entity<Worker>()
-            .HasMany(w => w.Qualifications)
-            .WithOne(q => q.Workers)
-            .HasForeignKey(q => q.WorkerId);
+            
+
+modelBuilder.Entity<Worker>()
+    .HasMany(w => w.Qualifications)
+    .WithMany()
+    .UsingEntity<WorkerQualification>(
+        j => j.HasOne(wq => wq.Qualification)
+            .WithMany()
+            .HasForeignKey(wq => wq.QualificationId),
+        j => j.HasOne(wq => wq.Worker)
+            .WithMany()
+            .HasForeignKey(wq => wq.WorkerId),
+        j =>
+        {
+            j.HasKey(wq => new { wq.WorkerId, wq.QualificationId });
+            j.ToTable("WorkerQualification");
+        }
+    );
 
         modelBuilder.Entity<Worker>()
             .HasMany(w => w.WorkerProjects)
