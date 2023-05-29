01ec54f81c0546a89d5d01a8b46228e156b58c8e
Kevin Finger <kfinger85@gmail.com>
2023-05-29 14:18:34 -0600

    CompanyMangerService begin definitions of actions

--- Services/CompanyManagerService.cs
+++ Services/CompanyManagerService.cs
@@ -24,5 +24,73 @@ namespace CompanyManager.Services
             _qualificationRepository = qualificationRepository;
         }
 
+        public void AssignWorkerToProject(int workerId, int projectId)
+        {
+            // Fetch the worker and the project from the database
+            Worker worker = _workerRepository.GetById(workerId);
+            Project project = _projectRepository.GetById(projectId);
+
+            // Call a method on the Worker entity to assign it to the project
+            worker.AddProject(project);
+
+            // Update the worker and the project in the database
+            _workerRepository.Update(worker);
+            _projectRepository.Update(project);
+
+            // Save changes to the database
+            _workerRepository.SaveChanges();
+            _projectRepository.SaveChanges();
+        }
+        public void UnassignWorkerFromProject(int workerId, int projectId)
+        {
+            try{
+                // Fetch the worker and the project from the database
+                Worker worker = _workerRepository.GetById(workerId);
+                Project project = _projectRepository.GetById(projectId);
+
+                // Call a method on the Worker entity to unassign it from the project
+                worker.RemoveProject(project);
+
+                // Update the worker and the project in the database
+                _workerRepository.Update(worker);
+                _projectRepository.Update(project);
+
+                // Save changes to the database
+                _workerRepository.SaveChanges();
+                _projectRepository.SaveChanges();
+            }
+            catch(DbUpdateException e){
+                throw new DbUpdateException("Worker is not assigned to this project", e);
+            }
+        }
+
+        public void startProject(int projectId){
+            Project project = _projectRepository.GetById(projectId);
+            project.SetStatus(ProjectStatus.ACTIVE);
+            _projectRepository.Update(project);
+            _projectRepository.SaveChanges();
+        }
+        public void startProject(String project)
+        {
+            Project project1 = _projectRepository.GetByName(project);
+            project1.SetStatus(ProjectStatus.ACTIVE);
+            _projectRepository.Update(project1);
+            _projectRepository.SaveChanges();
+        }
+
+        public void finishProject(int projectId){
+            Project project = _projectRepository.GetById(projectId);
+            project.SetStatus(ProjectStatus.FINISHED);
+            _projectRepository.Update(project);
+            _projectRepository.SaveChanges();
+        }
+        public void finishProject(String projectName)
+        {
+            Project project = _projectRepository.GetByName(projectName);
+            project.SetStatus(ProjectStatus.FINISHED);
+            _projectRepository.Update(project);
+            _projectRepository.SaveChanges();
+        }
     }
+
 }
