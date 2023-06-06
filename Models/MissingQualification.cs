e5dc8ab35b892d33188e0ee1169d42331f597e74
Kevin Finger <kfinger85@gmail.com>
2023-06-05 22:35:24 -0600

    Add MissingQs Entity

---
+++ Models/MissingQualification.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations.Schema;
+namespace CompanyManager.Models
+{
+    public class MissingQualification
+    {
+        public long ProjectId { get; set; }
+
+        public long QualificationId { get; set; }
+
+        public virtual String Name { get; set; }
+
+        [NotMapped]
+        public virtual Project Project { get; set; }
+        [NotMapped]
+        public virtual Qualification Qualification { get; set; }
+        public MissingQualification() { }
+        public MissingQualification(Project project, Qualification qualification, String name)
+        {
+            Project = project;
+            Qualification = qualification;
+            Name = name;
+        }
+        public static MissingQualification QualificationToMissingQualification(Project project, Qualification qualification)
+        {
+            return new MissingQualification(project, qualification, qualification.Name); // assuming Qualification has a property 'Name'
+        }
+    }
+}
