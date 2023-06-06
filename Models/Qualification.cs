765ce763eff67487138bb1f10834e68c12bb9d73
Kevin Finger <kfinger85@gmail.com>
2023-06-05 22:35:39 -0600

    Reactor Qs Entity

--- Models/Qualification.cs
+++ Models/Qualification.cs
@@ -1,110 +1,48 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using CompanyManager.Models.DTO;
 
 namespace CompanyManager.Models
 {
     public class Qualification
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
-
-        [Required]
-        [Column(name: "name")]
         public string Name { get; set; }
-
         public virtual ICollection<Project> Projects { get; set; } = new HashSet<Project>();
+        public virtual ICollection<Worker> Workers { get; set; } = new HashSet<Worker>();
 
-        public virtual ICollection<Worker> Workers {get; set; }= new HashSet<Worker>();
-        public virtual ICollection<Company> Companies { get; set; } = new HashSet<Company>();
-
-        // public virtual ICollection<WorkerQualification> WorkerQualifications { get; set; } = new HashSet<WorkerQualification>();
-
-        protected Qualification() 
-        { 
-        }
-
-        public Qualification(string description)
+        public Qualification()
         {
-            if (string.IsNullOrEmpty(description) || IsAllBlankSpace(description))
+        }
+        public Qualification(string name)
+        {
+            Name = name;
+        }
+        public static Qualification GetQualification(string name)
+        {
+            return new Qualification(name);
+        }
+        public void AddWorker(Qualification workerQualification, Worker worker)
+        {
+            if (workerQualification == null)
             {
-                throw new ArgumentException("Description must not be null or empty");
+                throw new ArgumentNullException(nameof(workerQualification));
             }
-            Workers = new HashSet<Worker>();
-            Name = description;
-        }
 
-        public void SetName(string description)
-        {
-            if (string.IsNullOrEmpty(description) || IsAllBlankSpace(description))
-            {
-                throw new ArgumentException("Description must not be null or empty");
-            }
-            Name = description;
-        }
-
-        private bool IsAllBlankSpace(string description)
-        {
-            return string.IsNullOrWhiteSpace(description);
-        }
-
-        public override bool Equals(object other)
-        {
-            if (other == null)
-            {
-                return false;
-            }
-            if (GetType() != other.GetType())
-            {
-                return false;
-            }
-            Qualification q = (Qualification)other;
-            return q.Name.Equals(Name);
-        }
-
-        public override int GetHashCode()
-        {
-            return Name.GetHashCode();
-        }
-
-        public override string ToString()
-        {
-            return Name;
-        }
-
-        public IEnumerable<Worker> GetWorkers()
-        {
-            return new HashSet<Worker>(Workers);
-        }
-
-        public void AddWorker(Worker worker)
-        {
             if (worker == null)
             {
-                throw new ArgumentNullException("Worker must not be null");
+                throw new ArgumentNullException(nameof(worker));
             }
+
+            if (!workerQualification.Equals(this))
+            {
+                throw new ArgumentException("The provided qualification is different from this qualification.");
+            }
+
+            if (Workers.Contains(worker))
+            {
+                throw new ArgumentException("The worker is already associated with this qualification.");
+            }
+
+            worker.Qualifications.Add(this);
             Workers.Add(worker);
-        }
-
-        public void RemoveWorker(Worker worker)
-        {
-            if (worker == null)
-            {
-                throw new ArgumentNullException("Worker must not be null");
-            }
-            Workers.Remove(worker);
-        }
-
-        public QualificationDTO ToDTO()
-        {
-            return new QualificationDTO(
-                Name,
-                Workers.Select(worker => worker.Name).ToArray()
-            );
         }
     }
 }
