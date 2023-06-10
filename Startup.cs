0a4fb7fd9ecd3106b3c4c8726a91363c0b13ae8c
Kevin Finger <kfinger85@gmail.com>
2023-06-10 15:52:15 -0600

    ApplicationUser reg, ProjectService reg, UseMiddleware CustomMiddleware, UseAuthentication, Logger

--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,20 @@
 using System.Runtime.InteropServices;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+
 using CompanyManager.Repositories;
 using CompanyManager.Services;
+using CompanyManager.Middleware;
+using CompanyManager.Models; 
+
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
+using Serilog;
+
+
+using CompanyManager.Logging;
 
 namespace CompanyManager
 {
@@ -33,11 +44,36 @@ namespace CompanyManager
             services.AddScoped<ICompanyRepository, CompanyRepository>();
             services.AddScoped<IWorkerRepository, WorkerRepository>();
             services.AddScoped<IProjectRepository, ProjectRepository>(); 
+
             services.AddScoped<WorkerService, WorkerService>();
-            services.AddScoped<CompanyService, CompanyService>();
+            services.AddScoped<ILogger<ProjectService>, Logger<ProjectService>>();
             services.AddScoped<ProjectService, ProjectService>();
+            services.AddScoped<CompanyService, CompanyService>();
             services.AddScoped<QualificationService, QualificationService>();
-            
+
+            services.AddScoped<CompanyInitializer, CompanyInitializer>();
+
+             services.AddIdentity<ApplicationUser, IdentityRole>()
+        .AddEntityFrameworkStores<CompanyManagerContext>()
+        .AddDefaultTokenProviders();
+
+
+            services.AddScoped<ProjectService>(serviceProvider =>
+            {
+                var context = serviceProvider.GetRequiredService<CompanyManagerContext>();
+                var workerService = serviceProvider.GetRequiredService<WorkerService>();
+                var logger = serviceProvider.GetRequiredService<ILogger<ProjectService>>();
+                return new ProjectService(context, workerService, logger);
+            });
+
+  
+
+            services.AddSingleton<ClientIpEnricher>();
+
+            services.AddLogging();
+
+
+            services.AddHttpContextAccessor();
 
 
             services.AddCors(options =>
@@ -57,29 +93,45 @@ namespace CompanyManager
                 {
                     options.JsonSerializerOptions.MaxDepth = 64; // Increase the maximum allowed depth
                 });
+            services.AddControllers();
+                services.AddLogging(loggingBuilder =>
+                    loggingBuilder.AddSerilog(dispose: true));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, CompanyManagerContext dbContext)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, CompanyManagerContext dbContext, IHostApplicationLifetime applicationLifetime,
+            CompanyInitializer companyInitializer)
         {
+            var clientIpEnricher = app.ApplicationServices.GetRequiredService<ClientIpEnricher>();
+
+
+            Logger.Configure(clientIpEnricher);
+            
+
             dbContext.Database.EnsureDeleted();
+            dbContext.Database.EnsureCreated();
 
             app.UseCors("AllowReactApp");
+            app.UseMiddleware<CustomMiddleware>();
+
             // app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            // app.UseStaticFiles();
 
             app.UseRouting();
 
+            app.UseAuthentication(); 
             app.UseAuthorization();
+
+            companyInitializer.Initialize(); // Assuming you have an Initialize method
 
                 // Apply pending migrations and create the database
                 // dbContext.Database.Migrate();
-            dbContext.Database.EnsureCreated();
 
-                CompanyInitializer companyInitializer = new CompanyInitializer(dbContext);
-                companyInitializer.Initialize();
-                // app.UseStaticFiles(); // For the wwwroot folder
-
+                app.UseStaticFiles(new StaticFileOptions
+                {
+                    FileProvider = new PhysicalFileProvider(
+                        Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")),
+                });
 
             app.UseEndpoints(endpoints =>
             {
@@ -88,6 +140,16 @@ namespace CompanyManager
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            applicationLifetime.ApplicationStarted.Register(() =>
+            {
+                Logger.LogInformation("Application started");
+            });
+            applicationLifetime.ApplicationStopping.Register(() =>
+            {
+                Logger.LogInformation("Application is stopping");
+            });
+
         }
     }
 }
