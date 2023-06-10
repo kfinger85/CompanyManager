8d2530a8159510558184976bfdee0e3519c9ffd5
Kevin Finger <kfinger85@gmail.com>
2023-06-10 15:49:32 -0600

    Services DIs, div of responsibilities

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -1,8 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using System.Text.Json;
+
+
 using CompanyManager.Models;
 using CompanyManager.Services;
+using CompanyManager.Logging;
+
 using Microsoft.EntityFrameworkCore;
 
 namespace CompanyManager.Controllers
@@ -11,26 +15,26 @@ namespace CompanyManager.Controllers
     {
         private readonly CompanyManagerContext _context;
         private readonly CompanyService _companyService;
-        public ProjectController(CompanyManagerContext context, CompanyService companyService)
+        private readonly WorkerService _workerService;
+        private readonly ProjectService _projectService;
+
+        public ProjectController(CompanyManagerContext context, CompanyService companyService, WorkerService workerService, ProjectService projectService)
         {
             _context = context;
             _companyService = companyService;
+            _workerService = workerService;
+            _projectService = projectService;
         }
+
         [HttpGet("projects")]
         public IActionResult Index()
         {
-        var projects = _context.Projects
-            .Select(p => new {
-                MissingQualifications = p.MissingQualifications.Select(mq => mq.Name),
-                Size = p.Size.ToString(),
-                p.Name,
-                Status = p.Status.ToString(),
-                Qualifications = p.Qualifications.Select(q => q.Name),
-                Workers = _context.WorkerProject
-                    .Where(wp => wp.ProjectId == p.Id)
-                    .Select(wp => wp.Worker.Name).ToList()
-            })
-            .ToList();
+            var projects = _projectService.FetchAllProjectDTOs();
+
+            if (projects == null)
+            {
+                return NotFound("Projects not found.");
+            }
             
             Debug.WriteLine(JsonSerializer.Serialize(projects)); // Log the JSON object
             return Ok(projects);
@@ -60,86 +64,118 @@ namespace CompanyManager.Controllers
             Debug.WriteLine(JsonSerializer.Serialize(project)); // Log the JSON object
             return Ok(project);
         }
-        [HttpPost("projects/assign")]
+        [HttpPut("projects/assign")]
         public IActionResult Assign([FromBody] AssignDTO assignDTO)
         {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
             try{
             var project = assignDTO.ProjectName;
             var worker = assignDTO.WorkerName;
 
-            var projectToAssign = _context.Projects
-                .IncludeAllProjectRelations()
-                .FirstOrDefault(p => p.Name == project);
-
-            var workerToAssign = _context.Workers
-                .IncludeAllWorkerRelations()
-                .FirstOrDefault(w => w.Name == worker);
-
-            if (projectToAssign == null)
+            var projectToAssignTo = _projectService.GetProjectByName(project);
+            var workerToAssign = _workerService.GetWorkerByName(worker);
+            if (projectToAssignTo == null)
             {
-                return NotFound("Project not found.");
+                return NotFound("Project not found in database.");
             }
-
             if (workerToAssign == null)
             {
-                return NotFound("Worker not found.");
+                return NotFound("Worker not found in database.");
             }
 
-            _companyService.AssignWorkerToProject(workerToAssign, projectToAssign);
-            _context.SaveChanges();
+            _companyService.AssignWorkerToProject(workerToAssign, projectToAssignTo);
 
 
-            return Ok();
+            return new OkResult();
+
             }catch(Exception e){
                 return BadRequest(e.Message);
-            }
-
-         
+            }        
 
     }
-        [HttpPost("projects/unassign")]
+        [HttpPut("projects/unassign")]
         public IActionResult UnassignWorkerFromProject([FromBody] AssignDTO assignDTO)
         {
             var project = assignDTO.ProjectName;
             var worker = assignDTO.WorkerName;
 
-            var projectToUnassign = _context.Projects
-                .IncludeAllProjectRelations()
-                .FirstOrDefault(p => p.Name == project);
+            var projectToUnassignFrom = _projectService.GetProjectByName(project);
+            var workerToUnassign = _workerService.GetWorkerByName(worker);
 
-            var workerToUnassign = _context.Workers
-                .IncludeAllWorkerRelations()
-                .FirstOrDefault(w => w.Name == worker);
+            return _projectService.UnassignWorkerFromProject(projectToUnassignFrom, workerToUnassign);
 
-            if (projectToUnassign == null || workerToUnassign == null)
-            {
-                return NotFound("Project or worker not found.");
-            }
-
-            var workerProject = _context.WorkerProject
-                .FirstOrDefault(wp => wp.WorkerId == projectToUnassign.Id && wp.ProjectId == workerToUnassign.Id);
-            if (workerProject == null)
-            {
-               return NotFound("Worker not assigned to project.");
-            }
-
-            foreach (var qualification in workerToUnassign.Qualifications)
-            {
-                var projectQualification = projectToUnassign.Qualifications.FirstOrDefault(q => q.Id == qualification.Id);
-                if (projectQualification != null)
-                {
-                    projectToUnassign.MissingQualifications.Add(new MissingQualification(projectToUnassign, qualification, qualification.Name));
-                }
-            }
-
-            _context.WorkerProject.Remove(workerProject);
-            projectToUnassign.WorkerProjects.Remove(workerProject);
-            workerToUnassign.WorkerProjects.Remove(workerProject);
-            _context.SaveChanges();
-
-
-            return Ok();
         }
+        [HttpPost("projects/start")]
+        public IActionResult StartProject([FromBody] JsonElement jsonData)
+        {
+            try{
+                string projectName = jsonData.GetProperty("projectName").GetString();
+                return ChangeProjectStatus(projectName, ProjectStatus.ACTIVE, "Project is already started.");
+            }catch(Exception e){
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost("projects/finish")]
+        public IActionResult FinishProject([FromBody] JsonElement jsonData)
+        {
+            try{
+            string projectName = jsonData.GetProperty("projectName").GetString();
+            return ChangeProjectStatus(projectName, ProjectStatus.FINISHED, "Project is already finished.");
+            }catch(Exception e){
+                return BadRequest(e.Message);
+            }
+        }
+
+        public IActionResult ChangeProjectStatus(string projectName, ProjectStatus newStatus, string alreadyInStatusMessage)
+        {
+            var projectToChange = _context.Projects
+                .IncludeAllProjectRelations()
+                .FirstOrDefault(p => p.Name == projectName);
+
+            var (result, success) = ProjectStatusCheck(projectToChange);
+            if (!success)
+            {
+                return result;
+            }
+            else
+            {
+                if (projectToChange.Status == newStatus)
+                {
+                    Logger.LogInformation($"{alreadyInStatusMessage} {projectToChange.Name}.");
+                    return BadRequest(alreadyInStatusMessage);
+                }
+
+                projectToChange.Status = newStatus;
+                _context.SaveChanges();
+
+                Logger.LogInformation($"Changed status of project {projectToChange.Name} to {newStatus}.");
+
+                return Ok();
+            }
+        }
+
+        public (IActionResult, bool) ProjectStatusCheck(Project project)
+        {
+            if (project == null)
+            {
+                Logger.LogInformation($"Project {project?.Name} not found.");
+                return (NotFound("Project not found."), false);
+            }
+
+            if (project.MissingQualifications.Any())
+            {
+                Logger.LogInformation($"Project {project.Name} cannot be started because it is missing qualifications.");
+                return (BadRequest("Project cannot be started because it is missing qualifications."), false);
+            }
+            return (null, true);
+        }
+
+
         public class AssignDTO
         {
             public string ProjectName { get; set; }
@@ -148,17 +184,17 @@ namespace CompanyManager.Controllers
 
     }
         public static class QueryExtensions
-    {
-        public static IQueryable<Project> IncludeAllProjectRelations(this IQueryable<Project> query)
         {
-            return query
-                .Include(p => p.WorkerProjects)
-                    .ThenInclude(wp => wp.Worker)
-                        .ThenInclude(w => w.Qualifications)
-                .Include(p => p.Company)
-                .Include(p => p.Qualifications)
-                .Include(p => p.MissingQualifications);
-        }
+            public static IQueryable<Project> IncludeAllProjectRelations(this IQueryable<Project> query)
+            {
+                return query
+                    .Include(p => p.WorkerProjects)
+                        .ThenInclude(wp => wp.Worker)
+                            .ThenInclude(w => w.Qualifications)
+                    .Include(p => p.Company)
+                    .Include(p => p.Qualifications)
+                    .Include(p => p.MissingQualifications);
+            }
 
         public static IQueryable<Worker> IncludeAllWorkerRelations(this IQueryable<Worker> query)
         {
