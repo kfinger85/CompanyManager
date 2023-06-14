95da8fd8c788a6de7376e2c4c09eb2c39e6a7491
Kevin Finger <kfinger85@gmail.com>
2023-06-14 14:37:11 -0600

    ProductCategory defining new Nav Properties

--- MusicProduction/Models/ProductCategory.cs
+++ MusicProduction/Models/ProductCategory.cs
@@ -4,13 +4,27 @@ public class ProductCategory
 {
     public int ProductCategoryId { get; set; }
     public string Name { get; set; }
-
     public int? ParentProductCategoryId { get; set; }
-    public ProductCategory ParentProductCategory { get; set; }
 
+
+    /*
+    If all ParentProductCategoryId values are NULL, that means every category in your database 
+    is currently considered a top-level category, with no parent category. 
+    */
+    public ProductCategory ParentProductCategory { get; set; }
     public ICollection<ProductCategory> SubCategories { get; set; }
-    public ICollection<Product> Products { get; set; }
+    public ICollection<Product> Products { get; set; }  // For ParentProductCategory relationship
+    public ICollection<Product> SubCategoryProducts { get; set; }  // For SubProductCategory relationship
+
+    public ProductCategory()
+    {
+        SubCategories = new HashSet<ProductCategory>();
+        Products = new HashSet<Product>();
+        SubCategoryProducts = new HashSet<Product>();  // Initialize it
+    }
 }
+
+
 
 
 }
