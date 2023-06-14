5e99d00963fe4c51f73dc9e6d8df865fb4080acb
Kevin Finger <kfinger85@gmail.com>
2023-06-14 14:36:09 -0600

    Refactors to define navigation props

--- MusicProduction/Models/Product.cs
+++ MusicProduction/Models/Product.cs
@@ -2,16 +2,27 @@ namespace  MusicProduction.Models
 {
     public class Product
     {
-        public int ProductId { get; set; }
-        public string Make { get; set; }
-        public string Model { get; set; }
-        public string SerialNumber { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-        public int Stock { get; set; }
-        public int ProductCategoryId { get; set; }
-        public ProductCategory ProductCategory { get; set; }
-        public ProductCategories SubCategory { get; set; }
-        public ICollection<StageProduct> StageProducts { get; set; }
+    public int ProductId { get; set; }
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public string SerialNumber { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+
+    public int ParentProductCategoryId { get; set; }
+    public int? SubProductCategoryId { get; set; }
+
+    public ProductCategory ParentProductCategory { get; set; }
+    public ProductCategory SubProductCategory { get; set; }
+
+
+
+
+    public Product()
+    {
     }
+    }
+
+
 }
