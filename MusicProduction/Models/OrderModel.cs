88df60405db2bab296003e1e15ebc12a4acd605c
Kevin Finger <kfinger85@gmail.com>
2023-06-16 23:20:09 -0600

    Order Model updates

--- MusicProduction/Models/OrderModel.cs
+++ MusicProduction/Models/OrderModel.cs
@@ -1,14 +1,32 @@
 
+using Bogus.DataSets;
+
 namespace MusicProduction.Models
 {
     public class Order
     {
         public int OrderId { get; set; }
+        public int CustomerId { get; set; }
         public string CustomerName { get; set; }
-        public DateTime OrderDate { get; set; }
+
+        public DateTime StartOrderDate { get; set; }
+
+        public DateTime EndOrderDate { get; set; }
+
+        public DateTime OrderCreated { get; set; }
+        public DateTime? OrderUpdated { get; set; }
 
         // Navigation property
         public ICollection<OrderLineItem> OrderLineItems { get; set; }
+        public Order()
+        {
+            OrderLineItems = new List<OrderLineItem>();
+            OrderCreated = DateTime.Now;
+            OrderUpdated = null;
+        }
+
     }
+
+    
 
 }
